714fedfb2e5608fe75db2b4726244dc0fee27975
Nate Festinger <nate.festinger@gmail.com>
2017-10-21 11:56:20 -0700

    Get hover working

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,10 @@
-const mouse = new THREE.Vector2();
 
 const CAMERA_RADIUS = 100; // radius of camera from center scene
 const FOV = 70;
 const CAMERA_NEAR = 1;
 const CAMERA_FAR = 10000;
 
-const BACKGROUND_CLR = 0xf0f0f0;
+const BACKGROUND_CLR = 0x000000;
 const LIGHT_CLR = 0xffffff;
 const LIGHT_INTENSITY = 1;
 
@@ -16,7 +15,10 @@ const BOX_SCALE_VARIANCE = 0.5;
 const TWO_PI = 2 * Math.PI;
 const THETA_ADVANCE = 0.1;
 
+const mouse = new THREE.Vector2();
+
 let camera, raycaster, renderer, scene;
+let intersected = null;
 
 let theta = 0;
 init();
@@ -31,12 +33,14 @@ function init() {
     FOV, aspect, CAMERA_NEAR, CAMERA_FAR
   );
 
+  camera.position.y = CAMERA_RADIUS;
+
   scene = new THREE.Scene();
   scene.background = new THREE.Color(BACKGROUND_CLR);
 
   // add light source
   const light = new THREE.DirectionalLight(LIGHT_CLR, LIGHT_INTENSITY);
-  light.position.set(1, 1, 1).normalize();
+  light.position.set(0, 1, 0).normalize();
   scene.add(light);
 
   const geometry = new THREE.BoxBufferGeometry(20, 20, 20);
@@ -70,7 +74,8 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   container.appendChild(renderer.domElement);
 
-  window.addEventListener('resize', onWindowResize)
+  window.addEventListener('resize', onWindowResize);
+  document.addEventListener('mousemove', onDocumentMouseMove);
 }
 
 function onWindowResize() {
@@ -92,9 +97,39 @@ function render() {
 
   // update camera position
   camera.position.x = CAMERA_RADIUS * Math.sin(THREE.Math.degToRad(theta));
-  camera.position.y = CAMERA_RADIUS * Math.cos(THREE.Math.degToRad(theta));
+  // camera.position.y = CAMERA_RADIUS * Math.cos(THREE.Math.degToRad(theta));
   camera.position.z = CAMERA_RADIUS * Math.cos(THREE.Math.degToRad(theta));
-  console.log(camera.position);
   camera.lookAt(scene.position);
+  camera.updateMatrixWorld();
+  console.log(camera.position);
+
+  // find and handle mouse intersections
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  console.log(mouse);
+
+  if (intersects.length > 0) {
+    if (intersected !== intersects[0].object) {
+      if (intersected) intersected.material.emissive.setHex(intersected.currentHex);
+
+      intersected = intersects[0].object;
+      intersected.currentHex = intersected.material.emissive.getHex();
+
+      scene.background = intersected.material.color;
+      intersected.material.emissive.setHex(0x999999);
+    }
+  } else if (intersected) {
+    intersected.material.emissive.setHex(intersected.currentHex);
+    scene.background = new THREE.Color(BACKGROUND_CLR);
+    intersected = null;
+  }
+
   renderer.render(scene, camera);
+}
+
+function onDocumentMouseMove(event) {
+  event.preventDefault();
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
